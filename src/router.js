a21edecc15fcb2f8a219c5d3974ed7fd03aa4298
benmfoster <bfoster@marlboro.edu>
2019-07-02 16:39:43 -0400

    adds router link to resume index

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Show from './views/Show.vue'
+import Index from './views/Index.vue'
 
 Vue.use(Router)
 
@@ -11,8 +12,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'resumes-index',
+      component: Index
     },
     {
       path: '/resumes/:id',
