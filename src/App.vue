e7716b8fcbc195753cc01874790a760b68a32f05
benmfoster <bfoster@marlboro.edu>
2019-07-02 16:45:58 -0400

    adds router links to resumes index and student portal

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> | <router-link to="/resumes/:id">Show</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link> | <router-link to="user/login">Student Portal</router-link>
     </div>
     <router-view/>
   </div>
