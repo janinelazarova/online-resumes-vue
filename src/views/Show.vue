3512995a9e8f638d2d5255285cd3180f948a4d20
IsraelChicago <rabbi.yi.hass@gmail.com>
2019-07-05 15:39:03 -0400

    Adds Show page

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -21,27 +21,31 @@
     <div class="row align-items-center">
       <div class="col">
         <small>First Name:</small> 
-        <h4 {{ student.first_name }}</h4> 
+        <h4>{{ student.first_name }}</h4> 
       </div>
+
       <div class="col">
         <small>Last Name:</small> 
          <h4>{{ student.last_name }}</h4>
       </div>
+
       <div class="col">
         <small>Email:</small> 
         <h4>{{ student.email }}</h4>
       </div>
+
       <div class="col">
         <small>Phone Number:</small> 
         <h4>{{ student.phone_number }}</h4>
       </div>
+
     </div>
   </div>
   <br>
   <br>
   <br>
 
-  <div>
+  
     <div class="container">
       <div class="row align-items-center">
         <div class="col">
@@ -51,103 +55,169 @@
         </div>
       </div>
     </div>
-  </div>
-  <br>
-  <br>
+    <br>
+    <br>
 
-<div class="container">
-    <div class="row align-items-center">
-      <div class="col">
-        <small>LinkedIn URL:</small> 
-        <br>
-        <h6> {{ student.linked_in }} </h6>
-      </div>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col">
+          <small>LinkedIn URL:</small> 
+          <br>
+          <h6> {{ student.linked_in }} </h6>
+        </div>
       
-      <div class="col">
-        <small>Twitter Handle:</small> 
-        <br>
-       <h6> {{ student.twitter }} </h6>
+        <div class="col">
+          <small>Twitter Handle:</small> 
+          <br>
+          <h6> {{ student.twitter }} </h6>
+        </div>
       </div>
-      </div>
-  </div>
-  <br>
+    </div>
+    <br>
 
-<div class="container">
-    <div class="row align-items-center">
-    
+    <div class="container">
+      <div class="row align-items-center">
         <div class="col">
           <small>Personal Blog/Website URL:</small>
           <br> 
-         <h6> {{ student.website }} </h6>
+          <h6> {{ student.website }} </h6>
         </div>
+        
         <div class="col">
           <small>Online Resume URL:</small>
           <br> 
-         <h6> {{ student.resume }} </h6>
+          <h6> {{ student.resume }} </h6>
         </div>
+
         <div class="col">
           <small>Github URL:</small> 
           <br>
          <h6> {{ student.github }} </h6>
+        </div>
+
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div>
+      <h3> Capstone</h3>
+    </div>
+    <br>
+
+    <div class="container">
+      <div class="row align-items-start">
+        <div class="col" id="student.capstone">
+    
+          <small>Name:</small> 
+          <h5> {{ student.capstone.first.name  }} </h5>
+          <small>Description:</small>
+          <h5> {{ student.capstone.first.description }} </h5>
+          <small>URL:</small>
+          <h5> {{ student.capstone.first.url }} </h5> 
+          <small> Screenshot:  (Optional)</small>
+          <br>
+          <img v-bind:src="student.capstone.first.screenshot">
+        </div>
+
+        <div class="col" id="student.capstone">
+    
+          <small>Name:</small> 
+          <h5> {{ student.capstone.second.name  }} </h5>
+          <small>Description:</small>
+          <h5> {{ student.capstone.second.description }} </h5>
+          <small>URL:</small>
+          <h5> {{ student.capstone.second.url }} </h5> 
+          <small> Screenshot:  (Optional)</small>
+          <br>
+          <img v-bind:src="student.capstone.second.screenshot">
+        </div>
+
+        <div class="col" id="student.capstone">
+    
+          <small>Name:</small> 
+          <h5> {{ student.capstone.first.name  }} </h5>
+          <small>Description:</small>
+          <h5> {{ student.capstone.first.description }} </h5>
+          <small>URL:</small>
+          <h5> {{ student.capstone.first.url }} </h5> 
+          <small> Screenshot:  (Optional)</small>
+          <br>
+          <img v-bind:src="student.capstone.first.screenshot">
         </div>
       </div>
     </div>
     <br>
     <br>
     <br>
-
-<div class="container">
-    <div class="row align-items-start">
-  <div class="col" id="student.capstone">
-    <h3> Capstone</h3>
-    <small>Name:</small> 
-    <h5> {{ student.capstone.name  }} </h5>
-    Description:
-     <h5> {{ student.capstone.description }} </h5>
-    URL:
-     <h5> {{ student.capstone.url }} </h5> 
-    <small> Screenshot:  (Optional)</small>
     <br>
-    <img v-bind:src="student.capstone.screenshot">
-  </div>
+    <br>
+    <br>
 
-  <div class="col" id="student.education">
-    <h3>Education</h3>
-    <small> Start Date: </small>
-    <h5> {{ student.education.start_date  }} </h5>
-    <small> End Date: </small>
-    <h5> {{ student.education.end_date }} </h5>
-    <small> Degree: </small>
-    <h5> {{ student.education.degree }} </h5>
-    <small> University Name: </small>
-    <h5> {{ student.education.university }} </h5>
-    <small> Details: </small>
-    <h5> {{ student.education.details }} </h5>
-  </div>
+    <div>
+      <h3> Education</h3>
+    </div>
+    
+    <div class="container">
+      <div class="row align-items-start">
+        <div class="col" id="student.education">
+          <small> Start Date: </small>
+          <h5> {{ student.education.start_date  }} </h5>
+          <small> End Date: </small>
+          <h5> {{ student.education.end_date }} </h5>
+          <small> Degree: </small>
+          <h5> {{ student.education.degree }} </h5>
+          <small> University Name: </small>
+          <h5> {{ student.education.university }} </h5>
+          <small> Details: </small>
+          <h5> {{ student.education.details }} </h5>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    
+    <div>
+      <h3> Experiance</h3>
+    </div>
+    
+    <div class="container">
+      <div class="row align-items-start">
+        <div class="col" id="student.experience">
+          <small> Start Date: </small>
+          <h5> {{ student.experiance.start_date  }} </h5>
+          <small> End Date: </small>
+          <h5> {{ student.experiance.end_date }} </h5>
+          <small> Title: </small>
+          <h5> {{ student.experiance.title }} </h5>
+          <small> Company Name: </small>
+          <h5> {{ student.experiance.company_name }} </h5>
+          <small> Details: </small> 
+          <h5> {{ student.experiance.details }} </h5>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
 
-  <div class="col" id="student.experience">
-    <h3>Experiance</h3>
-    <small> Start Date: </small>
-    <h5> {{ student.experience.start_date  }} </h5>
-    <small> End Date: </small>
-    <h5> {{ student.experience.end_date }} </h5>
-    <small> Title: </small>
-    <h5> {{ student.experience.title }} </h5>
-    <small> Company Name: </small>
-    <h5> {{ student.experience.company_name }} </h5>
-    <small> Details: </small> 
-    <h5> {{ student.experience.details }} </h5>
-  </div>
-
-  <div class="col" id="student.skills">
-    <h3>Skills</h3>
-    <small> Name: </small>
-    <h5> {{ student.skills.name  }} </h5>
-  </div>
-
-  </div>
-</div>
-  <br>
+    <div>
+      <h3>Skills</h3>
+    </div>
+    
+    <div class="container">
+      <div class="row align-items-start">
+        <div class="col" id="student.skills">
+          
+          <small> Name: </small>
+          <h5> {{ student.skills.name  }} </h5>
+        </div>
+      </div>
+    </div>
+    <br>
 
   
 
@@ -181,10 +251,24 @@ export default {
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQBgZsX5yrGZAbwdWcJAyQ8Z4UIx0-QVMNZe6TvOrrkPci-3N_Uw87N9o",
         
         capstone: {
-          name: "Bit-Please",
-          description: "Actualize coding bootcamp",
-          url: "anyonecanlearntocode.com",
-          screenshot: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjKjJ51adXD7gTLh2VUV1Dqow5-5QJi4rGJPLK59aYmkrNgjytQCiPPsfG"
+          first: {
+            name: "Bit-Please",
+            description: "Actualize coding bootcamp",
+            url: "anyonecanlearntocode.com",
+            screenshot: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjKjJ51adXD7gTLh2VUV1Dqow5-5QJi4rGJPLK59aYmkrNgjytQCiPPsfG"
+            },
+          second: {
+            name: "Bit-Please",
+            description: "Actualize coding bootcamp",
+            url: "anyonecanlearntocode.com",
+            screenshot: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjKjJ51adXD7gTLh2VUV1Dqow5-5QJi4rGJPLK59aYmkrNgjytQCiPPsfG"
+            },
+          third: {
+            name: "Bit-Please",
+            description: "Actualize coding bootcamp",
+            url: "anyonecanlearntocode.com",
+            screenshot: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjKjJ51adXD7gTLh2VUV1Dqow5-5QJi4rGJPLK59aYmkrNgjytQCiPPsfG"
+            }
           
           // student_id
           },
