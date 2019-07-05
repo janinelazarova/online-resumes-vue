3902c3e8e940c97ed7d7323f1f7fb4d2fd7b5ba9
benmfoster <bfoster@marlboro.edu>
2019-07-05 15:11:51 -0400

    adds search, bootstrap nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> | <router-link to="user/login">Student Portal</router-link>
-    </div>
     <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand">Resumes</a>
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</nav>
+      <a class="navbar-brand"><router-link to="/">Resumes App</router-link></a>
+      View student resumes from potential applicants from all over the anglosphere! View student education, experience, capstone projects, skills.
+      <a class="navbar-brand"><router-link to="/">Student Portal</router-link></a>
+      <autocomplete :search="search" placeholder="Search Student Resumes" aria-label="Search"></autocomplete>
+    </nav>
     <router-view/>
   </div>
 </template>
@@ -35,3 +31,33 @@
   color: #42b983;
 }
 </style>
+
+<script>
+  // npm install --save @trevoreyre/autocomplete-vue
+  import Autocomplete from '@trevoreyre/autocomplete-vue';
+  import '@trevoreyre/autocomplete-vue/dist/style.css';
+  import axios from 'axios';
+
+  export default {
+  data: function() {
+    return {
+    students: [] 
+    };
+  },
+  created: function() {
+    axios.get("/api/students").then(response => {
+      this.students = response.data;
+      console.log(this.students);
+    });
+  },
+  methods: {
+    search(input) {
+      if (input.length < 1) { return [] }
+      return this.students.filter(student => {
+        return student["last_name"].toLowerCase()
+          .startsWith(input.toLowerCase())
+      })
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios";
+import Autocomplete from '@trevoreyre/autocomplete-vue'
+import '@trevoreyre/autocomplete-vue/dist/style.css'
 
 axios.defaults.baseURL = 
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+  process.env.NODE_ENV === "development" ? "https://bit-please-team-api.herokuapp.com" : "/"
 
-
-
-
+Vue.use(Autocomplete)
 Vue.config.productionTip = false
 
 new Vue({
